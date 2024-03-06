adef5754618bcf10ea6de8410eb7b634356f176c
Deucébé <nakhoy.blyad@kurwa.ru>
2024-03-06 10:08:27 +0100

    Integration Bootstrap

--- app/index.php
+++ app/index.php
@@ -4,8 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>onihilist's Webbase</title>
+    <!-- CSS stylesheets -->
+    <link rel="stylesheet" type="txt/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="txt/css" href="vendor/twbs/bootstrap-icons/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="txt/css" href="css/styles.css">
 </head>
 <body>
-    <h1>Sup y'all this is my webbase !</h1>
+    <div class="container">
+        <h1><i class="bi bi-git"></i>Sup y'all this is my webbase !</h1>
+        <p>
+            Et voilà une petite balise &lt;p&gt; !
+        </p>
+    </div>
 </body>
 </html>
